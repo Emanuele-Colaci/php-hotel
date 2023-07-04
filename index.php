2251ef6fdf8e6bf6942cb97588679d6329dcbc42
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-07-04 14:46:03 +0200

    Fatto esercizio senza style

--- index.php
+++ index.php
@@ -1,54 +1,27 @@
 <?php
 
-    $hotels = [
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-    ];
+    include __DIR__. '../partials/hotels.php';
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
-    <title>php-hotel</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/style.css">
+        <title>php-hotel</title>
+    </head>
+    <body>
+        <?php foreach($hotels as $hotel){ ?>
+            <div>
+                <h2><?php echo $hotel['name']; ?></h2>
+                <p><?php echo $hotel['description']; ?></p>
+                <p>Parcheggio: <?php echo $hotel['parking'] ? 'Disponibile' : 'Non disponibile'; ?></p>
+                <p>Voto: <?php echo $hotel['vote']; ?></p>
+                <p>Distanza dal centro: <?php echo $hotel['distance_to_center']; ?> km</p>
+            </div>
+        <?php } ?>
+    </body>
 </html>
